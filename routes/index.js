07617ea4f9c11ebaab1afea6e547d808ff9633e8
Moses Okemwa <okemwamoses@gmail.com>
2019-09-01 22:07:42 +0300

    token verification works on protected view

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,7 @@
 var express = require('express');
-var { validationResult } = require('express-validator');
-var verifyToken  = require('../middleware/authMiddleware')
+var homeController = require('../controllers/homeController')
 
 var router = express.Router();
-
-router.get('/', verifyToken, function (req, res, next) {
-    var errors = validationResult(req)
-
-    // Check if there were errors from the form.
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
-    }
-    res.status(200).send({authorized: true, msg: "welcome home"})
-  })
+router.get('/home', homeController.home_page_get )
 
 module.exports = router;
