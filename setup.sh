a671465c7b28523cf5c9454cd78c6f63a115171d
Moses Okemwa <okemwamoses@gmail.com>
2019-09-04 16:34:45 +0300

    installation update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@ function jwtSecret() {
     python -c 'import random; print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))'
 }
 
+echo "[GIT] cloning hacker-bay project....."
 git clone git@github.com:mosesokemwa/hackerbay-backend.git
-cd hackerbay-backend
+alias proj="cd hackerbay-backend"
+proj
+echo "[hacker-bay] creating a jwtSecret key....."
 echo "jwtSecret=$(jwtSecret)" >> .env
+echo "[hacker-bay] installing project dependancies....."
 npm install
